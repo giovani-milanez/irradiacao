ee4907b84b51fe3122f99ca74a36b8a59ebd1b62
Giovani Milanez Espindola <giovani.milanez@gmail.com>
2024-11-20 10:58:19 -0300

    env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   env: {
-    NEXT_PUBLIC_API_URL: "http://127.0.0.1:8080"
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
   }
 };
 
